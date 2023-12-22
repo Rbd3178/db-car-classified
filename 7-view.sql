fffad80cb7aeb6d0e6e0cc3e80ccdfc28c516ff9
bananananacat <112201735+bananananacat@users.noreply.github.com>
2023-12-22 22:55:57 +0300

    Add files via upload
    7, work in progress

---
+++ 7-view.sql
@@ -0,0 +1,14 @@
+drop schema if exists db_car_classified_view cascade;
+
+create schema db_car_classified_view;
+
+set search_path = db_car_classified_view, public;
+
+--- users
+
+drop view if exists clients;
+create view clients as
+select u.user_id as user_id,
+       u.name as users_name,
+       overlay(u.phone::text placing '+7(***)***-**-' from 2 for 9) as phone
+from db_car_classified.users as u;
